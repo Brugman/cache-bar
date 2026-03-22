9e20c7e7e6067140a823e1a8e221eedb8d6206cd
Tim Brugman <tim@timbrugman.com>
2026-03-22 02:26:05 +0100

    Add partial support for OPcache Manager
    Ref #28

---
+++ supported-plugins/opcache-manager.php
@@ -0,0 +1,29 @@
+<?php
+
+defined( 'ABSPATH' ) || exit;
+
+$clear_url = add_query_arg(
+    '_wpnonce',
+    wp_create_nonce( 'quick-action-opcm-tools' ),
+    admin_url( 'admin.php?page=opcm-tools&quick-action=reset' )
+);
+
+return [
+    'slug'  => 'opcache-manager/opcache-manager.php',
+    'id'    => 'opcache_manager',
+    'name'  => 'OPcache Manager',
+    'links' => [
+        'clear' => [
+            'label' => 'Clear PHP OPcache',
+            'url'   => $clear_url,
+        ],
+        'settings' => [
+            'label' => 'Settings',
+            'url'   => admin_url( 'admin.php?page=opcm-tools' ),
+        ],
+    ],
+    'rn'    => [
+        'perfopsone-dashboard',
+    ],
+];
+
